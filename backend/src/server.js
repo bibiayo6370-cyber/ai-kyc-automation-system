d8e95dcea9547eed3f0ea1dd3c47aaebeb30542a
Babajide Ibiayo <babajideibiayo@yahoo.com>
2026-06-17 18:26:16 +0100

    Initiaised Express backend and MongoDB connection

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/database");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "AI KYC Automation System API Running"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

ed8873ab242d312850d2955ff4b2394bdd59272f
Babajide Ibiayo <babajideibiayo@yahoo.com>
2026-06-18 15:25:10 +0100

    created User Model

---
+++ backend/src/models/Users.js
@@ -0,0 +1,44 @@
+import mongoose from mongoose;
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 100
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["customer", "admin"],
+    default: "customer"
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive", "suspended"],
+    default: "active"
+  },
+}, {
+  timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;

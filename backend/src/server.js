442d5a0c761475907905b9f8a9a7d11d91f694fa
Babajide Ibiayo <b.ibiayo6370@miva.edu.ng>
2026-07-15 15:46:27 +0100

    Updated server port configuration to use numeric environment values

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ app.get("/api/v1", (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);

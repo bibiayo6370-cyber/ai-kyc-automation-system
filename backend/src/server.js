1773a5863a14c06e227da8412d0150729f12faac
Babajide Ibiayo <b.ibiayo6370@miva.edu.ng>
2026-07-16 12:19:27 +0100

    Added dotenv library

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express, { json } from "express";
 import cors from "cors";
+
+import 'dotenv/config';
 import { APP_NAME } from "./config/constants.js";
 import authRoutes from "./routes/authRoutes.js";
 

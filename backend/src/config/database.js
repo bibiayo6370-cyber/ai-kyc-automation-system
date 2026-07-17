c8b7810dcedb6ff60e93d1ba1d7ea917782a7361
Babajide Ibiayo <b.ibiayo6370@miva.edu.ng>
2026-07-17 11:16:53 +0100

    Modified MongoDB connection message and updated relevant screenshot

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    // console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("MongoDB Connected:");
   } catch (error) {
     console.error(error.message);
     process.exit(1);
